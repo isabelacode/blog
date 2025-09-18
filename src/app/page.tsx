31292f83c1180faae4d07e7de0ba63b3b1a05822
Isabela Cristina <isabeladigitalcircle@gmail.com>
2025-09-18 10:54:34 -0300

    Add Link and Image components to HomePage; update Header to use Link for navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
+import Link from "next/link";
+import Image from "next/image";
 import { Suspense } from "react";
 
 export default async function HomePage() {
@@ -9,6 +11,24 @@ export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
+          <Image
+            className="group-hover:scale-105 transition"
+            src="/images/bryen_0.png"
+            width={1200}
+            height={720}
+            alt="Título do post"
+          />
+        </Link>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
+          dolorem est dolor porro, doloribus neque, quidem mollitia doloremque,
+          ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam harum blanditiis
+          mollitia?
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
